7c87c3b8cc49a95b52d3b8b7a2337e44581ea764
Harshit Khandelwal <harshita.khandelwal@aveva.com>
2018-11-28 16:40:01 +0530

    committing changes except css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,24 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+
+  boxes:number[]=[];
+  counter:number=0;
+
+  OnAddbtnClick(){
+    this.counter++;
+    this.boxes.push(this.counter);
+  }
+
+  OnRemovebtnClick(){
+    if(this.counter!=0)
+    {
+      this.counter--;
+      this.boxes.pop();
+    }
+  }
 }
+ 
